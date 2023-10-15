fe25acb733422a26db4d39e55a22e92fd5d2e12a
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-10-15 12:57:33 -0500

    fixed exports and finds() in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 
 const numberOfFriends = async () =>
   User.aggregate()
@@ -7,130 +7,73 @@ const numberOfFriends = async () =>
 
 module.exports = {
   // GET ALL users
-  async getUsers(req, res) {
-    try {
-    console.log("Getting Users")
+  getUsers(req,res) {
     User.find()
-      .select('-__v')
-      .then((userDB) => {
-        res.json(userDB);
-      })
-    } catch(err) {
+    .then(async (users) => {
+        const userObj = {
+            users,
+            numberOfFriends: await numberOfFriends(),
+        };
+        return res.json(userObj);
+    })
+    .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
-      };
-  },
+        return res.status(500).json(err);
+    });
+},
 
   // get single user by id 
-  async getSingleUser(req, res) {
-    try {
+  getSingleUser(req,res) {
     User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      .populate('friends')
-      .populate('thoughts')
-      .then((userDB) => {
-        if(userDB) {
-          res.json({ userDB })
-        } else{
-          res.status(404).json({ message: 'User not found' })
-        }
-      })
-    } catch(err)  {
-        console.log(err);
-        res.status(500).json(err);
-      };
-  },
+    .select('-__v')
+    .then(async (user) => 
+    !user 
+        ? res.status(404).json({message: 'No User with that id found'})
+        : res.json({
+            user
+        })
+    ) 
+    .catch((err) => {
+        console.log(err)
+        return res.status(500).json(err);
+    })
+},
+
   // create a new post
-  async createUser(req, res) {
-    try {
-    console.log("CREATE USER")
+  createUser(req,res) {
     User.create(req.body)
-    console.log(req.body, "REQ BODY")
-      .then((userDB) => res.json(userDB))
-      // console.log(userDB)
-    } catch(err) { res.status(500).json(err);
-    }
-  },
+    .then((user) => res.json(user))
+    .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err)
+    });
+},
 
 //   update user by id
-  async updateUser(req, res) {
-    try {
-    User.findOneAndUpdate(
+updateUser(req,res) {
+  User.findOneAndUpdate(
       { _id: req.params.userId },
-      {
-        $set: req.body,
-      },
-      {
-        runValidators: true,
-        new: true,
-      }
-    )
-      // return user data if not return the err
-      .then((userDB) => {
-        if (!userDB) {
-          return res.status(404).json({ message: 'User does not exist' });
-        }
-        res.json(userDB);
-      })
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-      };
-  },
+      { $set: req.body },
+      { runValidators: true, new: true }
+  )
+  .then((user) =>
+  !user
+    ? res.status(404).json({ message: 'No user with this id!' })
+    : res.json(user)
+)
+.catch((err) => res.status(500).json(err));
+},
+
    // delete user by id 
-   async deleteUser(req, res) {
-    try {
+   deleteUser(req,res) {
     User.findOneAndDelete({ _id: req.params.userId })
-      .then((userDB) => {
-        if (!userDB) {
-          return res.status(404).json({ message: 'User does not exist' });
-        }
-        // deleting all the thoughts associated with user id
-        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-      })
-      .then(() => {
-        res.json({ message: 'User and the thought associated are deleted' });
-      })
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-      };
-  },
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : User.deleteOne({ _id: req.params.User })
+    )
+    .then(() => res.json({ message: 'User and friends deleted!' }))
+    .catch((err) => res.status(500).json(err));
+}
 
-  // add friend 
-  async addFriend(req, res) {
-    try {
-    User.findOneAndUpdate({ _id: req.params.userId },
-      { $addToSet: { friends: req.params.friendId } },
-      { new: true })
-      .then((userDB) => {
-        if (!userDB) {
-          return res.status(404).json({ message: 'User does not exist' });
-        }
-        res.json(userDB);
-      })
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-      };
-  },
-  // remove friend associated with a specific user
-  async removeFriend(req, res) {
-    try {
-    User.findOneAndUpdate({ _id: req.params.userId },
-      { $pull: { friends: req.params.friendId } },
-      { new: true })
-      .then((userDB) => {
-        if (!userDB) {
-          return res.status(404).json({ message: 'User does not exist' });
-        }
-        res.json(userDB);
-      })
-    } catch(err)  {
-        console.log(err);
-        res.status(500).json(err);
-      };
-  },
 };
-
-
